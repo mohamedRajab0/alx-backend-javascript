4a5dce1b8dc457320b816900cdc144e39ae156af
mohamedRajab0 <elzimmohamed@gmail.com>
2024-08-09 07:14:20 +0300

    check

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,16 @@
+import Car from './10-car.js';
+
+export default class EVCar extends Car {
+    constructor(brand, motor, color, range) {
+        super(brand, motor, color);
+        this._range = range;
+    }
+
+    cloneCar() {
+        const clonedCar = new Car();
+        clonedCar._brand = this._brand;
+        clonedCar._motor = this._motor;
+        clonedCar._color = this._color;
+        return clonedCar;
+    }
+}
